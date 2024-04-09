66049aa634adfe8c97f89d30d230b8598ab52959
9Tai_Nguyenduc <tai.nguyenduc@smartdev.com>
2024-04-09 23:11:25 +0700

    pdf to ocr

--- lib/convert/from/from_pdf.dart
+++ lib/convert/from/from_pdf.dart
@@ -77,7 +77,8 @@ class FromPdfMethods {
       if (response.statusCode == 200) {
         return ConvertSuccessResponse.fromMap(response.data);
       } else {
-        throw Exception('Error ${response.statusCode}: ${response.statusMessage}');
+        throw Exception(
+            'Error ${response.statusCode}: ${response.statusMessage}');
       }
     } on DioException catch (e) {
       throw Exception('Error converting file: $e');
@@ -150,7 +151,8 @@ class FromPdfMethods {
       if (response.statusCode == 200) {
         return ConvertSuccessResponse.fromMap(response.data);
       } else {
-        throw Exception('Error ${response.statusCode}: ${response.statusMessage}');
+        throw Exception(
+            'Error ${response.statusCode}: ${response.statusMessage}');
       }
     } on DioException catch (e) {
       throw Exception('Error converting file: $e');
@@ -207,7 +209,8 @@ class FromPdfMethods {
       if (response.statusCode == 200) {
         return ConvertSuccessResponse.fromMap(response.data);
       } else {
-        throw Exception('Error ${response.statusCode}: ${response.statusMessage}');
+        throw Exception(
+            'Error ${response.statusCode}: ${response.statusMessage}');
       }
     } on DioException catch (e) {
       throw Exception('Error converting file: $e');
@@ -264,7 +267,65 @@ class FromPdfMethods {
       if (response.statusCode == 200) {
         return ConvertSuccessResponse.fromMap(response.data);
       } else {
-        throw Exception('Error ${response.statusCode}: ${response.statusMessage}');
+        throw Exception(
+            'Error ${response.statusCode}: ${response.statusMessage}');
+      }
+    } on DioException catch (e) {
+      throw Exception('Error converting file: $e');
+    }
+  }
+
+  /// Convert PDF to OCR
+  /// The API for converting PDF documents to Text files.
+  /// Read more about the converter here: https://www.convertapi.com/pdf-to-docx
+  Future<ConvertApiResponse> toOcr({
+    /// File to convert
+    required File file,
+
+    /// Store converted file on our secure server and provides download URL.
+    bool storeFile = false,
+
+    /// Converted output file name without extension. The extension will be added automatically.
+    String? fileName,
+
+    /// Sets the password to open protected documents.
+    String? password,
+
+    /// Conversion timeout in seconds.
+    int timeout = 600,
+
+    /// Enable optical character recognition(OCR).
+    bool enableOcr = true,
+
+    /// Set page range. Example 1-10 or 1,2,5. Default is 1-2000.
+    String pageRange = '1-2000',
+  }) async {
+    final formData = FormData.fromMap({
+      'StoreFile': storeFile,
+      'File': await MultipartFile.fromFile(
+        file.path,
+        // filename: (file.path.split('/').last).split('.').first,
+        filename: file.path.split('/').last,
+        contentType: MediaType('application', 'pdf'),
+      ),
+      'FileName': fileName ?? (file.path.split('/').last).split('.').first,
+      'Timeout': timeout.toString(),
+      'Password': password ?? '',
+      'PageRange': pageRange,
+      'OcrLanguage': 'en',
+    });
+
+    try {
+      final response = await client.post(
+        path: '/convert/pdf/to/ocr',
+        data: formData,
+      );
+
+      if (response.statusCode == 200) {
+        return ConvertSuccessResponse.fromMap(response.data);
+      } else {
+        throw Exception(
+            'Error ${response.statusCode}: ${response.statusMessage}');
       }
     } on DioException catch (e) {
       throw Exception('Error converting file: $e');
