6e9dc2e6240f9ea404fe9ab7fe89d56e2ceb1c87
9Tai_Nguyenduc <tai.nguyenduc@smartdev.com>
2024-04-11 19:15:54 +0700

    Update from_docx.dart

--- lib/convert/from/from_docx.dart
+++ lib/convert/from/from_docx.dart
@@ -158,4 +158,78 @@ class FromDocxMethods {
       throw Exception('Error converting file: $e');
     }
   }
+
+  /// Convert DOCX to Pdf
+  /// The API for converting Word documents to Text files.
+  /// Read more about the converter here: https://www.convertapi.com/doc-to-pdf
+  Future<ConvertApiResponse> toPdf({
+    /// File to convert
+    required File file,
+
+    /// Store converted file on our secure server and provides download URL.
+    bool storeFile = false,
+
+    /// Converted output file name without extension. The extension will be added automatically.
+    String? fileName,
+
+    /// Sets the password to open protected documents.
+    String? password,
+
+    /// Conversion timeout in seconds.
+    int timeout = 900,
+
+    /// Replace similar symbols with their counterparts in a text file, such as a copyright symbol with (c).
+    bool substitutions = false,
+
+    /// Inserts line breaks at the end of each line of text.
+    bool lineBreaks = true,
+
+    /// Specifies what encoding the parser should use when converting text files.
+    TextEncoding textEncoding = TextEncoding.auto,
+
+    /// Set end of line character. The character which will be used to break lines.
+    EndLineChar endLineChar = EndLineChar.crlf,
+
+    /// Specifies the format to use when saving a document.
+    SaveFormat saveFormat = SaveFormat.unicodetext,
+  }) async {
+    final formData = FormData.fromMap({
+      'Timeout': timeout.toString(),
+      'FileName': fileName ?? (file.path.split('/').last).split('.').first,
+      'Password': password ?? '',
+      'File': await MultipartFile.fromFile(
+        file.path,
+        filename: file.path.split('/').last,
+        contentType: MediaType(
+          'application',
+          'dox',
+        ),
+      ),
+      'StoreFile': storeFile,
+      'PageRange': 1,
+      'ConvertMarkups': false,
+      'ConvertTags': false,
+      'ConvertMetadata': true,
+      'ConvertHeadings': true,
+      'ConvertBookmarks': false,
+      'UpdateToc': false,
+      'CompressPDF': false,
+    });
+
+    try {
+      final response = await client.post(
+        path: '/convert/docx/to/pdf',
+        data: formData,
+      );
+
+      if (response.statusCode == 200) {
+        return ConvertSuccessResponse.fromMap(response.data);
+      } else {
+        throw Exception(
+            'Error ${response.statusCode}: ${response.statusMessage}');
+      }
+    } on DioException catch (e) {
+      throw Exception('Error converting file: $e');
+    }
+  }
 }
